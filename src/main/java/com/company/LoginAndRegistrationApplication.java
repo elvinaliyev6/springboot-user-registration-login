ca237b932f8f55a6b00fd17220b2005fd9d33c92
elvinaliyev6 <aliyevaliyevelvin@gmail.com>
2022-04-14 19:00:54 +0400

    login and registration in spring boot

---
+++ src/main/java/com/company/LoginAndRegistrationApplication.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginAndRegistrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginAndRegistrationApplication.class, args);
+        //17.42
+    }
+
+}
