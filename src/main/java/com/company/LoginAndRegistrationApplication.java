a480b62f75ab11925763e98202a31939d123348b
elvinaliyev6 <aliyevaliyevelvin@gmail.com>
2022-04-15 13:02:53 +0400

    login and registration in spring boot

--- src/main/java/com/company/LoginAndRegistrationApplication.java
+++ src/main/java/com/company/LoginAndRegistrationApplication.java
@@ -8,7 +8,6 @@ public class LoginAndRegistrationApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LoginAndRegistrationApplication.class, args);
-        System.out.println("...");
     }
 
 }
